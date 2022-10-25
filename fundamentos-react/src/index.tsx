a978d092a25b508c125a39036c9eb06ef3d5b4d6
Genivaldo <genivaldo.serra@live.com>
2022-10-25 09:04:04 -0300

    Update index.tsx

--- fundamentos-react/src/index.tsx
+++ fundamentos-react/src/index.tsx
@@ -1,9 +1,12 @@
 import ReactDOM  from 'react-dom';
 import React  from 'react';
 
+const tag = <strong>React</strong>;
+
 ReactDOM.render(
     <div>
         <h1>Hello, world!</h1>
+        { tag }
     </div>,
     document.getElementById('root')
 );
