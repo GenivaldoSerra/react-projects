e4a2f023d1903c2ebc72b68185f12adc7ef8f942
Genivaldo <genivaldo.serra@live.com>
2022-10-25 09:27:26 -0300

    Update index.tsx

--- fundamentos-react/src/index.tsx
+++ fundamentos-react/src/index.tsx
@@ -2,12 +2,15 @@ import './index.css';
 import ReactDOM  from 'react-dom';
 import React  from 'react';
 
+import Primeiro from './components/basicos/Primeiro';
+
 const tag = <strong>React</strong>;
 
 ReactDOM.render(
     <div>
         <h1>Hello, world!</h1>
-        { tag }
+        { tag } <br />
+        <Primeiro />
     </div>,
     document.getElementById('root')
 );
