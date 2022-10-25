532cc5ad511af024f6987f2b7f13a822e82e0f31
Genivaldo <genivaldo.serra@live.com>
2022-10-25 09:34:33 -0300

    Update Primeiro.tsx

--- fundamentos-react/src/components/basicos/Primeiro.tsx
+++ fundamentos-react/src/components/basicos/Primeiro.tsx
@@ -1,5 +1,11 @@
 function Primeiro () {
-    return <span>Primeiro Componente!</span>
+    const msg = 'Seja bem vindo(a)!';
+    return (
+        <div>
+            <span>Primeiro Componente!</span>
+            <h2>{ msg }</h2>
+        </div>
+    );
 }
 
 export default Primeiro
