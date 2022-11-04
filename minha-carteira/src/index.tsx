619cb670fc22ec51fb468fd7e97946ecc67625e3
GenivaldoSerra <genivaldo.serra@live.com>
2022-11-04 11:19:44 -0300

    feat: Finalizado a estrutura básica do projeto.

--- minha-carteira/src/index.tsx
+++ minha-carteira/src/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+
+import Dashboards from './pages/Dashboards';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Dashboards />
   </React.StrictMode>
 );
