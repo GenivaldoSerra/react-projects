fddee5153164c8f02a6ab02347745e9b296ffa89
GenivaldoSerra <genivaldo.serra@live.com>
2022-11-04 16:34:55 -0300

    feat: Criando Componentes

--- minha-carteira/src/styles/GlobalStyles.ts
+++ minha-carteira/src/styles/GlobalStyles.ts
@@ -1,8 +1,23 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
-  width: 100%;
-  height: 100%;
-  margin: 0;
-  padding: 0;
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body, html, #root {
+    height: 100%;
+  }
+  
+  *, button, input {
+    border: 0;
+    outline: 0;
+    font-family: 'Roboto', sans-serif;
+  }
+
+  button {
+    cursor: pointer;
+  }
 `;
