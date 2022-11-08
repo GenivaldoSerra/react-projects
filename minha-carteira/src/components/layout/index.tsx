100297892c287a060eef90808fb0204a9f9c7e95
GenivaldoSerra <genivaldo.serra@live.com>
2022-11-08 07:40:05 -0300

    Componente Layout

---
+++ minha-carteira/src/components/layout/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container } from './styles';
+
+const Layout: React.FC = () => {
+  return (
+    <div>
+      <Container>
+        <h1>Layout</h1>
+      </Container>
+    </div>
+  );
+}
+
+export default Layout;
